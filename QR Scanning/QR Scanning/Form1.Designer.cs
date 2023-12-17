d3f7564e61f143591513f49e4c6ddb3e458144d1
AbdulmalekAli60 <98859591+AbdulmalekAli60@users.noreply.github.com>
2023-12-17 20:11:09 +0300

    Add files via upload

---
+++ QR Scanning/QR Scanning/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace QR_Scanning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(284, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 136);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 180);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(582, 230);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 447);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(250, 448);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(408, 449);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 52);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(583, 448);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 51);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Logo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 540);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ QR Scanning/QR Scanning/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder; //Downloaded
+
+
+namespace QR_Scanning
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)// Generate 
+        {
+            QRCodeGenerator QR = new QRCodeGenerator();
+            
+            QRCodeData data = QR.CreateQrCode(textBox1.Text, QRCodeGenerator.ECCLevel.Q); // for giving the data tho the QR code first parameter is the info i want to give to the QR code ECCL level is the level accept error
+            
+            QRCode code = new QRCode(data);// passing the data we want to the QRCode  we created
+
+            pictureBox1.Image = code.GetGraphic(5); // get graphics is used to show the image, the 5 is the qulaity of the image
+        }
+
+        private void button2_Click(object sender, EventArgs e)// save button
+        {
+            if(pictureBox1.Image != null) { 
+            String savePath = @"C:\Users\Zxshx\OneDrive\Documents\Imagesss\savedQR.png";
+            pictureBox1.Image.Save(savePath);
+            }
+            else
+            {
+                MessageBox.Show("error");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)//color
+        {
+            QRCodeGenerator QR = new QRCodeGenerator();
+
+            QRCodeData data = QR.CreateQrCode(textBox1.Text, QRCodeGenerator.ECCLevel.Q);  
+
+            QRCode code = new QRCode(data);
+
+            Bitmap QRImage = code.GetGraphic(20,Color.Red, Color.Green, true);//  1- color of QR code, 2 color of the border, true is for multy color
+
+            pictureBox1.Image = QRImage;
+        }
+
+        private void button4_Click(object sender, EventArgs e) // logo button 
+        {
+            QRCodeGenerator QR = new QRCodeGenerator();
+
+            QRCodeData data = QR.CreateQrCode(textBox1.Text, QRCodeGenerator.ECCLevel.Q);
+
+            QRCode code = new QRCode(data);
+
+            string path = @"C:\Users\Zxshx\OneDrive\Documents\Pictures\Screenshots\Screenshot 2023-09-18 102631.png"; 
+         
+            Bitmap QRImage = code.GetGraphic(20, Color.Red, Color.Green,(Bitmap) Bitmap.FromFile(path)); // type casting adn then pass the image it self
+
+            pictureBox1.Image = QRImage;
+        }
+    }
+}
